6931301064077ab3971a9e5a368cf4122ad03c9a
a <4184070+MrCurtis@users.noreply.github.com>
2017-10-17 13:33:39 +0200

    Add function to produce time series data.

--- met_data/historical_data/models.py
+++ met_data/historical_data/models.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from django.db import models
 
 from historical_data.met_data_getter import Region, Month, ValueType
@@ -28,6 +29,99 @@ value_type_mapper = {
     ValueType.MEAN_TEMP: "mean_temp",
     ValueType.SUNSHINE: "sunshine",
     ValueType.RAINFALL: "rainfall"}
+
+
+def get_time_series(value_type, regions):
+    """Gets time series data.
+
+    Args:
+      value_type: A valid ValueType enum.
+      regions: An iterable of Region enums.
+
+    Returns:
+      A dictionary representing the time-series for the given value type
+      and regions. The dictionary will have the following form:
+        {
+          "value_type": "<value_type>"
+          "labels": ["<date_1>", "<date_2>",...,"date_n"],
+          "series": [{
+            "name": "<region_name_1>",
+            "data": [<value_1>, <value_2>,...,<value_n>]
+          },{
+            "name": "<region_name_2>",
+            "data": [<value_1>, <value_2>,...,<value_n>]
+          },{
+            ...
+          },{
+            "name": "<region_name_m>",
+            "data": [<value_1>, <value_2>,...,<value_n>]
+          }]}
+    """
+    value_type_to_title_mapper = {
+        ValueType.MAX_TEMP: "Maximum Temperature",
+        ValueType.MIN_TEMP: "Minimum Temperature",
+        ValueType.MEAN_TEMP: "Mean Temperature",
+        ValueType.SUNSHINE: "Sunshine",
+        ValueType.RAINFALL: "Rainfall"}
+
+    month_to_lable_string_mapper = {
+        1: "JAN",
+        2: "FEB",
+        3: "MAR",
+        4: "APR",
+        5: "MAY",
+        6: "JUN",
+        7: "JUL",
+        8: "AUG",
+        9: "SEP",
+        10: "OCT",
+        11: "NOV",
+        12: "DEC"}
+
+    region_to_name_mapper = {
+        Region.UK: "UK",
+        Region.ENGLAND: "England",
+        Region.SCOTLAND: "Scotland",
+        Region.WALES: "Wales"}
+
+    entries = HistoricalData.objects\
+        .filter(region__in=[region_mapper[region] for region in regions])\
+        .exclude(**{value_type_mapper[value_type]+"__isnull": True})
+
+    potential_max = potential_min = (entries[0].year, entries[0].month)
+    dd = defaultdict(lambda: defaultdict(lambda: None))
+    for entry in entries:
+        potential_min = min((entry.year, entry.month), potential_min)
+        potential_max = max((entry.year, entry.month), potential_max)
+        dd[(entry.year, entry.month)][entry.region] = getattr(entry, value_type_mapper[value_type])
+
+    year_month_tuples = _create_list_of_tuples(potential_min, potential_max)
+
+    labels = [str(year)+"-"+month_to_lable_string_mapper[month] for year, month in year_month_tuples]
+
+    series = []
+    for region in regions:
+        d = {
+            "name": region_to_name_mapper[region],
+            "data": [dd[t][region_mapper[region]] for t in year_month_tuples]}
+        series.append(d)
+
+    return {
+        "value_type": value_type_to_title_mapper[value_type],
+        "labels": labels,
+        "series": series}
+    
+
+def _create_list_of_tuples(smallest, largest):
+    l = []
+    curr = smallest
+    while curr <= largest:
+        l.append(curr)
+        if curr[1] >= 12:
+            curr = (curr[0]+1, 1)
+        else:
+            curr = (curr[0], curr[1]+1)
+    return l
 
 
 def create_or_update_data_point(
--- met_data/historical_data/test_models.py
+++ met_data/historical_data/test_models.py
@@ -2,14 +2,95 @@ from django.test import TestCase
 from historical_data.models import (
         HistoricalData,
         create_or_update_data_point,
+        get_time_series,
         region_mapper,
         month_mapper,
         value_type_mapper)
 from historical_data.met_data_getter import  Month, Region, ValueType
+from historical_data.met_data_getter import  DataPoint
 from django.db.utils import IntegrityError
 
 
-class HistoricalDataManagerTests(TestCase):
+class GetTimeSeriesTests(TestCase):
+
+    def test_returns_with_correct_value_type(self):
+        expected_value_type = "Rainfall"
+        data_points = {
+            DataPoint(Region.WALES, 1234, Month.MAY, ValueType.RAINFALL, 23.5),        
+            DataPoint(Region.WALES, 1234, Month.JUN, ValueType.RAINFALL, 22.5)}
+        for dp in data_points:
+            create_or_update_data_point(*dp)
+
+        returned = get_time_series(ValueType.RAINFALL, {Region.WALES, Region.ENGLAND})
+
+        self.assertEqual(returned["value_type"], expected_value_type)
+
+    def test_labels_contains_every_month_between_earliest_and_latest_for_value_type(self):
+        expected_labels = [
+            "1913-MAY",
+            "1913-JUN",
+            "1913-JUL",
+            "1913-AUG",
+            "1913-SEP",
+            "1913-OCT",
+            "1913-NOV",
+            "1913-DEC",
+            "1914-JAN",
+            "1914-FEB",
+            "1914-MAR"]
+
+        data_points = {
+            DataPoint(Region.WALES, 1912, Month.MAY, ValueType.SUNSHINE, 45.5),        
+            DataPoint(Region.WALES, 1913, Month.MAY, ValueType.RAINFALL, 23.5),        
+            DataPoint(Region.WALES, 1913, Month.JUN, ValueType.RAINFALL, 22.5),
+            DataPoint(Region.ENGLAND, 1913, Month.AUG, ValueType.RAINFALL, 22.5),
+            DataPoint(Region.ENGLAND, 1913, Month.SEP, ValueType.RAINFALL, 22.5),
+            DataPoint(Region.ENGLAND, 1913, Month.DEC, ValueType.RAINFALL, 22.5),
+            DataPoint(Region.ENGLAND, 1914, Month.FEB, ValueType.RAINFALL, 22.5),
+            DataPoint(Region.ENGLAND, 1914, Month.MAR, ValueType.RAINFALL, 22.5)}
+        for dp in data_points:
+            create_or_update_data_point(*dp)
+
+        returned = get_time_series(ValueType.RAINFALL, {Region.WALES, Region.ENGLAND})
+
+        self.assertEqual(returned["labels"], expected_labels)
+
+    def test_returns_only_series_for_requested_regions(self):
+        expected_regions = {"Wales", "Scotland"}
+
+        data_points = {
+            DataPoint(Region.WALES, 1913, Month.MAY, ValueType.RAINFALL, 23.5),        
+            DataPoint(Region.WALES, 1913, Month.JUN, ValueType.RAINFALL, 22.5)}
+        for dp in data_points:
+            create_or_update_data_point(*dp)
+
+        returned = get_time_series(ValueType.RAINFALL, {Region.WALES, Region.SCOTLAND})
+        returned_regions = {x["name"] for x in returned["series"]}
+
+        self.assertEqual(returned_regions, expected_regions)
+
+    def test_returns_correst_value_series_filling_missing_data_with_none(self):
+        expected_value_series = [
+            None, None, None, 14.3, 12.8, None, None, 22.5, None, 32.5, None]
+
+        data_points = {
+            DataPoint(Region.WALES, 1912, Month.MAY, ValueType.SUNSHINE, 45.5),        
+            DataPoint(Region.WALES, 1913, Month.MAY, ValueType.MAX_TEMP, 23.5),        
+            DataPoint(Region.WALES, 1913, Month.JUN, ValueType.MAX_TEMP, 22.5),
+            DataPoint(Region.ENGLAND, 1913, Month.AUG, ValueType.MAX_TEMP, 14.3),
+            DataPoint(Region.ENGLAND, 1913, Month.SEP, ValueType.MAX_TEMP, 12.8),
+            DataPoint(Region.ENGLAND, 1913, Month.DEC, ValueType.MAX_TEMP, 22.5),
+            DataPoint(Region.ENGLAND, 1914, Month.FEB, ValueType.MAX_TEMP, 32.5),
+            DataPoint(Region.WALES, 1914, Month.MAR, ValueType.MAX_TEMP, 24.5)}
+        for dp in data_points:
+            create_or_update_data_point(*dp)
+
+        returned = get_time_series(ValueType.MAX_TEMP, {Region.WALES, Region.ENGLAND})
+        returned_value_series = [x["data"] for x in returned["series"] if x["name"] == "England"][0]
+
+        self.assertEqual(expected_value_series, returned_value_series)
+
+class CreateOrUpdateDataPointTests(TestCase):
 
     def test_create_or_update__creates_if_no_entry_for_time_and_region_exists(self):
         create_or_update_data_point(
