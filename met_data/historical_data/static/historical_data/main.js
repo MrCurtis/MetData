56fed5dcf57e71ded07bc8ec7d44e782072689bb
a <4184070+MrCurtis@users.noreply.github.com>
2017-10-17 20:28:04 +0200

    Create page displaying plot.

--- met_data/historical_data/static/historical_data/main.js
+++ met_data/historical_data/static/historical_data/main.js
@@ -1,6 +1,66 @@
-console.log("Is working")
-var data = {
-	labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-	series: [[5, 2, 4, 2, 0]]};
+update()
+var selectorPanel = $("#selector-panel");
+selectorPanel.change(update);
 
-new Chartist.Line('.ct-chart', data);
+function update(){
+    var value_type = getSelectedValueType();
+    var regions = getSelectedRegions();
+    getDataFromServer(value_type, regions)
+}
+
+function getSelectedValueType() {
+    return $('input[name=value-type]:radio:checked').val();
+};
+
+function getSelectedRegions(){
+    return $('input[name=region]:checkbox:checked').map(function() {
+	    return $(this).val();
+    }).get().join("-");
+    
+};
+
+function colourMapper(region){
+    console.log("region", region)
+    var map = {
+        "UK": "black",
+        "England": "blue",
+        "Scotland": "red",
+        "Wales": "green"
+    }
+    console.log(map[region])
+    return map[region]
+}
+
+function createDisplayData(data){
+    console.log("This bit running")
+    var labels = data["labels"];
+    var series = data["series"];
+    return series.map((s) => {
+        return {
+	    type: "scatter",
+	    mode: "lines",
+	    name: s["name"],
+	    x: labels,
+	    y: s["data"],
+	    line: {color: colourMapper(s["name"])}}
+    });
+};
+
+function success(data){
+    var plot_data = createDisplayData(data);
+    var layout = {
+        xaxis:{
+	    autorange: true,
+	    rangeslider: {range: [data["series"][0], data["series"][300]]},
+	    }
+        }
+    Plotly.newPlot('tester', plot_data, layout);
+};
+
+function getDataFromServer(value_type, regions){
+    $.ajax({
+        url: "http://localhost:8000/time-series/"+value_type+"/"+regions+"",
+        success: success,
+        dataType: "json"
+    });
+};
