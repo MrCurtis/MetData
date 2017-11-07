2a903423700e8100f5a478fa5822d534c74f891c
a <4184070+MrCurtis@users.noreply.github.com>
2017-11-07 19:37:14 +0100

    Tidy up JavaScript.

--- met_data/historical_data/static/historical_data/main.js
+++ met_data/historical_data/static/historical_data/main.js
@@ -1,66 +1,59 @@
-update()
-var selectorPanel = $("#selector-panel");
-selectorPanel.change(update);
+/**Retrieves data fron the server and displays it as a plot line-graph.*/
+(function (){
+	updatePlot();
+	$("#selector-panel").change(updatePlot);
 
-function update(){
-    var value_type = getSelectedValueType();
-    var regions = getSelectedRegions();
-    getDataFromServer(value_type, regions)
-}
+	function updatePlot(){
+	    var value_type = getSelectedValueType();
+	    var regions = getSelectedRegions();
+	    getDataFromServer(value_type, regions);
+	}
 
-function getSelectedValueType() {
-    return $('input[name=value-type]:radio:checked').val();
-};
+	function getSelectedValueType() {
+	    return $("input[name=value-type]:radio:checked").val();
+	}
 
-function getSelectedRegions(){
-    return $('input[name=region]:checkbox:checked').map(function() {
-	    return $(this).val();
-    }).get().join("-");
-    
-};
+	function getSelectedRegions(){
+	    return $("input[name=region]:checkbox:checked").map(function() {
+		return $(this).val();
+	    }).get().join("-");
+	}
 
-function colourMapper(region){
-    console.log("region", region)
-    var map = {
-        "UK": "black",
-        "England": "blue",
-        "Scotland": "red",
-        "Wales": "green"
-    }
-    console.log(map[region])
-    return map[region]
-}
+	var colourMapper = {
+	    "UK": "black",
+	    "England": "blue",
+	    "Scotland": "red",
+	    "Wales": "green"
+	};
 
-function createDisplayData(data){
-    console.log("This bit running")
-    var labels = data["labels"];
-    var series = data["series"];
-    return series.map((s) => {
-        return {
-	    type: "scatter",
-	    mode: "lines",
-	    name: s["name"],
-	    x: labels,
-	    y: s["data"],
-	    line: {color: colourMapper(s["name"])}}
-    });
-};
+	function getDataFromServer(value_type, regions){
+	    $.ajax({
+		url: "http://localhost:8000/time-series/"+value_type+"/"+regions,
+		success: plotData,
+		dataType: "json"
+	    });
+	}
 
-function success(data){
-    var plot_data = createDisplayData(data);
-    var layout = {
-        xaxis:{
-	    autorange: true,
-	    rangeslider: {range: [data["series"][0], data["series"][300]]},
-	    }
-        }
-    Plotly.newPlot('tester', plot_data, layout);
-};
+	function createDisplayData(data){
+	    return data.series.map((s) => {
+		return {
+		    type: "scatter",
+		    mode: "lines",
+		    name: s.name,
+		    x: data.labels,
+		    y: s.data,
+		    line: {color: colourMapper[s.name]}};
+	    });
+	}
 
-function getDataFromServer(value_type, regions){
-    $.ajax({
-        url: "http://localhost:8000/time-series/"+value_type+"/"+regions+"",
-        success: success,
-        dataType: "json"
-    });
-};
+	function plotData(data){
+	    var plot_data = createDisplayData(data);
+	    var layout = {
+		xaxis:{
+		    autorange: true,
+		    rangeslider: {range: [data.series[0], data.series[300]]}
+		    }
+		};
+	    Plotly.newPlot("plot", plot_data, layout);
+	}
+})();
